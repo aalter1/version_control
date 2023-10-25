13268914477bfac88d996c230387791932f6ae3f
aalter1 <148587809+aalter1@users.noreply.github.com>
2023-10-25 15:17:16 -0400

    Delete decode.py

--- decode.py
+++
@@ -1,8 +0,0 @@
-def decode(coded_password):
-    decoded_password = ""
-    for char in coded_password:
-        if int(char) < 3:
-            decoded_password += str(int(char) + 7)
-        else:
-            decoded_password += str(int(char) - 3)
-    return decoded_password
